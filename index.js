d2ff1e1389b3eb7df1e8ecca66de4546781b1321
William Phokompe <phokompe.william@outlook.com>
2019-09-19 11:46:49 +0200

    Configure GraphQL middleware functions

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const express = require('express'),
     // Func used in the place of an express middleware func.
     // It will take incoming req and funnel them through 
     // the GraphQL parser and redirect them to relevant Resolvers
-    graphqlHttp = require('express-graphql');
+    graphqlHttp = require('express-graphql'),
+    {} = require(); // 
 
 app.use(bodyParser.json());
 
-app.get('/', (request, response) => {
-    response.send('Hello World!');
-});
-
+// GraphQL Middleware function setup
+app.use('/graphql', graphqlHttp({
+    schema: null,
+    rootValue: {} // Contains all resolver functions
+}));
 app.listen(5000);
